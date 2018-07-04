675acf288c658dc2cc4934a61f0becf77731a22e
smart-07 <smart-07@smart-07-PC>
2018-07-04 16:25:15 +0900

    [박원우] 1.DriverDB

--- Restaurant/src/ownerPark/DriverDB.java
+++ Restaurant/src/ownerPark/DriverDB.java
@@ -1,5 +1,19 @@
 package ownerPark;
 
-public class DriverDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DriverDB {
+	public Connection driverDBcon() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String jdbcDriver = "jdbc:mysql://localhost:3306/foodmarket?" +
+				"useUnicode=true&characterEncoding=euckr";
+		String dbUser = "foodmarketid";
+		String dbPass = "foodmarketpw";
+		
+		Connection reconn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+		return reconn;
+	}
 }
