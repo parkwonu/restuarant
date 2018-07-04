ab5ac96204bfc162ac1027af3a9cc06fd00c4134
smart-09 <smart-09@smart-09-PC>
2018-07-04 16:17:40 +0900

    [김호순] member.java

--- Restaurant/src/ownerPark/Member.java
+++ Restaurant/src/ownerPark/Member.java
@@ -1,5 +1,60 @@
 package ownerPark;
 
 public class Member {
-
+	private String m_id;
+	private String m_pw;
+	private String m_name;
+	private String m_level;
+	private String m_email;
+	private String m_addr;
+	private String m_phone;
+	private String m_date;
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_level() {
+		return m_level;
+	}
+	public void setM_level(String m_level) {
+		this.m_level = m_level;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_addr() {
+		return m_addr;
+	}
+	public void setM_addr(String m_addr) {
+		this.m_addr = m_addr;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public String getM_date() {
+		return m_date;
+	}
+	public void setM_date(String m_date) {
+		this.m_date = m_date;
+	}
 }
