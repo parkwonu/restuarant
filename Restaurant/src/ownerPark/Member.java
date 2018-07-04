b9e18f74bf7b34417b598668074aae50684283aa
Administrator <Administrator@smart20111>
2018-07-04 16:23:43 +0900

    [이응빈] 1. Member.java

--- Restaurant/src/ownerPark/Member.java
+++ Restaurant/src/ownerPark/Member.java
@@ -3,12 +3,17 @@ package ownerPark;
 public class Member {
 	private String m_id;
 	private String m_pw;
+	private String m_pw1;
 	private String m_name;
 	private String m_level;
-	private String m_email;
+	private String m_email1;
+	private String m_email2;
 	private String m_addr;
-	private String m_phone;
+	private String m_phone1;
+	private String m_phone2;
+	private String m_phone3;
 	private String m_date;
+	
 	public String getM_id() {
 		return m_id;
 	}
@@ -20,6 +25,12 @@ public class Member {
 	}
 	public void setM_pw(String m_pw) {
 		this.m_pw = m_pw;
+	}
+	public String getM_pw1() {
+		return m_pw1;
+	}
+	public void setM_pw1(String m_pw1) {
+		this.m_pw1 = m_pw1;
 	}
 	public String getM_name() {
 		return m_name;
@@ -33,11 +44,17 @@ public class Member {
 	public void setM_level(String m_level) {
 		this.m_level = m_level;
 	}
-	public String getM_email() {
-		return m_email;
+	public String getM_email1() {
+		return m_email1;
 	}
-	public void setM_email(String m_email) {
-		this.m_email = m_email;
+	public void setM_email1(String m_email1) {
+		this.m_email1 = m_email1;
+	}
+	public String getM_email2() {
+		return m_email2;
+	}
+	public void setM_email2(String m_email2) {
+		this.m_email2 = m_email2;
 	}
 	public String getM_addr() {
 		return m_addr;
@@ -45,11 +62,23 @@ public class Member {
 	public void setM_addr(String m_addr) {
 		this.m_addr = m_addr;
 	}
-	public String getM_phone() {
-		return m_phone;
+	public String getM_phone1() {
+		return m_phone1;
 	}
-	public void setM_phone(String m_phone) {
-		this.m_phone = m_phone;
+	public void setM_phone1(String m_phone1) {
+		this.m_phone1 = m_phone1;
+	}
+	public String getM_phone2() {
+		return m_phone2;
+	}
+	public void setM_phone2(String m_phone2) {
+		this.m_phone2 = m_phone2;
+	}
+	public String getM_phone3() {
+		return m_phone3;
+	}
+	public void setM_phone3(String m_phone3) {
+		this.m_phone3 = m_phone3;
 	}
 	public String getM_date() {
 		return m_date;
@@ -57,4 +86,6 @@ public class Member {
 	public void setM_date(String m_date) {
 		this.m_date = m_date;
 	}
+	
+	
 }
